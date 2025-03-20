92d951b9c0fccd52520a5199cde031d6f4ddc39c
Vytautas Martusevicius <vytautas.martusevicius@satalia.com>
2025-03-20 10:23:06 +0200

    chore: fix startup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
 const app = require("./app");
 const connectDatabase = require("./config/database");
-const cloudinary = require("cloudinary");
-const logger = require("snore-log");
+const {v2: cloudinary} = require("cloudinary");
+const pino = require("pino");
+const logger = pino();
 const PORT = process.env.PORT || 4000;
 
 // UncaughtException Error
 process.on("uncaughtException", (err) => {
-  console.log(`Error: ${err.message}`);
+  logger.error(`Error: ${err.message}`);
   process.exit(1);
 });
 
@@ -16,15 +17,15 @@ cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
-}), logger();
+})
 
 const server = app.listen(PORT, () => {
-  console.log(`Server running`);
+  logger.info(`Server running`);
 });
 
 // Unhandled Promise Rejection
 process.on("unhandledRejection", (err) => {
-  console.log(`Error: ${err.message}`);
+  logger.error(`Error: ${err.message}`);
   server.close(() => {
     process.exit(1);
   });
